690844918175419b9efa411712be22e1868a1764
Javiera Hormazábal <93295581+brulee-e@users.noreply.github.com>
2024-07-03 02:20:06 -0400

    first commit

---
+++ app/products/page.tsx
@@ -0,0 +1,5 @@
+export default function ProductsPage() {
+  return (
+    <div>roducots page</div>
+  )
+}
---
+++ src/utils/index.ts
@@ -0,0 +1,6 @@
+export function formatCurrency(amount: number) {
+  return new Intl.NumberFormat("es-CL", {
+    style: "currency",
+    currency: "CLP",
+  }).format(amount);
+}
